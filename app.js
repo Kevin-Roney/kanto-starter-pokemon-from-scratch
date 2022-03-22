89210881f66d15f278f0729aa38b978e23c436e9
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-22 16:19:41 -0700

    got eventlistener and main page JS finished in app.js, images not working so figuring that out next

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
+import { getAllPokemans } from './fetch-utils.js';
+import { renderPokeCard } from './render-utils.js';
 
-// let state
-
+const pokeListContainer = document.querySelector('.pokemon-tile');
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+window.addEventListener('load', async () => {
+    const pokes = await getAllPokemans();
+    for (let poke of pokes) {
+        const pokeEl = renderPokeCard(poke);
+        pokeListContainer.append(pokeEl);
+    }
+});
